286fcffd91b608fd93f384c8beae3f4e0f9ca2ef
kristoffergjerde <kristoffergjerde@gmail.com>
2018-10-10 12:51:37 +0200

    Should work now. My error from last time was that I made random weights for each value in the input, but I only need one for each input.

--- A3/perceptron_training_algorithm.py
+++ A3/perceptron_training_algorithm.py
@@ -3,47 +3,67 @@ from random import random
 
 class Perceptron:
 
-    def __init__(self,num_inputs, length_input):
-        self.weights = [[round(random() - 0.5,1)for _ in range(length_input)] for _ in range(num_inputs)]
-        self.threshold = round(random() - 0.5,1)
+    def __init__(self,num_inputs):
+        self.weights = [random() - 0.5 for _ in range(num_inputs)]
+        self.threshold = random() - 0.5
         self.output = []
         self.learning_rate = 0.1
 
+        """
+        Example values from the book
+        
+        self.weights = [0.3, -0.1]
+        self.threshold = 0.2
+        self.output = []
+        self.learning_rate = 0.1
+        """
+
     def step(self,value):
-        return 1 if value > 0 else 0
+        return 1 if value >= 0 else 0
 
     def one_iteration(self, input, desired_out):
         out = []
         i = 0
         while i < len(input[0]):
             # Calculate output
-            temp = 0
-            for x in range(len(input)):
-                temp += input[x][i] * self.weights[x][i]
-            temp -= self.threshold
-            temp = self.step(temp)
-            out.append(temp)
+            temp_out = sum(input[x][i] * self.weights[x] for x in range(len(input))) - self.threshold
+            temp_out = self.step(temp_out)
+            out.append(temp_out)
 
             # Update weights
-            error = desired_out[i] - temp
+            error = desired_out[i] - temp_out
             for y in range(len(input)):
                 delta = self.learning_rate * input[y][i] * error
-                self.weights[y][i] = round(self.weights[y][i] + delta, 2)
+                self.weights[y] = self.weights[y] + delta
 
-            # Next value in input
+            # Check next
             i += 1
         self.output = out
         return out
 
-input = [
-    [0,0,1,1],
-    [0,1,0,1]]
-p = Perceptron(len(input),len(input[0]))
-desired_AND = [0,0,0,1]
-desired_OR = [0,1,1,1]
 
-times = 5
-for x in range(times):
-    res = p.one_iteration(input,desired_AND)
-    print(res)
+# Loop testing for several Perceptrons
+desired_AND = [0, 0, 0, 1]
+desired_OR = [0, 1, 1, 1]
+match = 0
+for x in range(1000):
+    input = [
+        [0, 0, 1, 1],
+        [0, 1, 0, 1]]
+    p = Perceptron(len(input))
+
+    res = []
+    counter = 20
+    while res != desired_AND and counter != 0:
+        res = p.one_iteration(input, desired_AND)
+        print(f"Out: {res}; Weights:{p.weights}; Thresh:{p.threshold}")
+        counter -= 1
+    if res == desired_AND:
+        match += 1
+    print()
+print(f"MATCHES: {match}")
+
+
+
+
 
